1bedd87bea252975150e456241f4a6ed8e799dd6
Neil Ling <neil.ling@aylaasia.com>
2020-12-01 18:13:09 +0800

    add login subcomponent and enable login flow injection

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.example.android.dagger.di
 
 import android.content.Context
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationComponent
 import dagger.BindsInstance
@@ -11,8 +12,12 @@ import javax.inject.Singleton
 @Component(modules = [StorageModule::class, AppSubcomponents::class])
 interface AppComponent {
 
+    // Classes that can be injected by this Component
     fun inject(activity: MainActivity)
+
+    // Types that can be retrieved from the graph
     fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
 
     @Component.Factory
     interface Factory {
